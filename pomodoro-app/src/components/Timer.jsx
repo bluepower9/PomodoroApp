0337e4b589f6b5faf7d40440f92138229d9e2149
admolani <admolani@gmail.com>
2024-10-25 16:43:18 -0700

    reformated from class to function. reduced line-count

--- pomodoro-app/src/components/Timer.jsx
+++ pomodoro-app/src/components/Timer.jsx
@@ -1,51 +1,22 @@
 import React from "react";
 import {useState, useEffect} from 'react';
 
-class Timer extends React.Component{
-    constructor(props){
-        super(props);
-        this.timer = 0;
+export default function Timer() {
+    const [time, setTime] = useState(5);
+    const countdown = setInterval(counter, 1000);
 
-        this.state = {
-            time: 5,
-            running: false
+    function counter() {
+        if (time > 0) {
+            setTime(time - 1);
+        }
+        else {
+            clearInterval(countdown);
         }
     }
 
-
-    componentDidMount(){
-        console.log('mounted')
-        this.startTimer();
-    }
-
-    startTimer(){
-        this.timer = setInterval(() => {this.countdown()}, 1000);
-        console.log('timer', this.timer);
-        console.log('starting timer');
-    }
-
-    countdown(){
-        if(this.state.time==0){
-            console.log('clearing')
-            clearInterval(this.timer)
-        }
-        else{
-            this.setState({
-                time: this.state.time-1
-            })
-        }
-
-    }
-
-    render(){
-
-        return (
-            <div class="bg-black w-60 content-center rounded-2xl">
-                <h1 class="flex justify-center text-white text-8xl pb-3">{this.state.time}</h1>
-            </div>
-        );
-    }
+    return (
+        <div class="bg-black w-60 content-center rounded-2xl">
+            <h1 class="flex justify-center text-white text-8xl pb-3">{time}</h1>
+        </div>
+    );
 }
-
-
-export default Timer;
