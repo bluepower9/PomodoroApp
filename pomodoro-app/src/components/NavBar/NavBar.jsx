8d9421d1ff91a6baa2d3282344d3c4dcec0deff2
bluepower9 <jarrod.leong@yahoo.com>
2024-10-30 22:26:32 -0700

    added filler logo

--- pomodoro-app/src/components/NavBar/NavBar.jsx
+++ pomodoro-app/src/components/NavBar/NavBar.jsx
@@ -6,6 +6,7 @@ export default function NavBar(props){
         <div class="fixed top-0 w-screen h-16 px-10 items-center justify-center bg-gray-800 backdrop-blur">
             <div class="relative max-w-6xl h-full mx-auto">
                 <div class="absolute flex w-fill left-0 top-0 bottom-0 justify-center items-center">
+                    <img src='./src/assets/pomopic.svg' class="max-h-full"/>
                     <div class="pt-1 text-white text-4xl px-2 font-heading">
                         <a href="">Pomodoro</a>
                     </div>
