317ed4d4ccf0d62001acc3e112f338b20a9f3968
bluepower9 <jarrod.leong@yahoo.com>
2024-11-02 22:41:02 -0700

    added initial notes display and functionality

--- pomodoro-app/src/components/Notes/Notes.jsx
+++ pomodoro-app/src/components/Notes/Notes.jsx
@@ -6,14 +6,25 @@ import NotesDisplay from "./NotesDisplay";
 export default function Notes(props){
     const [expanded, setexpanded] = useState(false);
 
+    const expIcon = () => {
+        return (expanded?
+            <path fill-rule="evenodd" stroke="currentColor" stroke-width=".75" d="M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707M15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707"/>
+            :
+            <path fill-rule="evenodd" stroke="currentColor" stroke-width=".75" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707"/>
+
+        );
+    }
 
     return (
         <div class="flex flex-col h-full w-full">
-            <div id="notes-header" class={`flex bg-gray-800 h-fit text-4xl px-3 pb-2 text-white hover:cursor-pointer ${expanded?"w-full rounded-t-xl": "w-fit rounded-xl"}`} onClick={()=>setexpanded(!expanded)}>
-                <h1 class="mr-4">Notes</h1>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="flex bi bi-pencil-square w-8 pt-2" viewBox="0 0 16 16">
+            <div class={`flex bg-gray-800 h-fit text-3xl px-3 pb-2 text-white hover:cursor-pointer ${expanded?"w-full rounded-t-xl": "w-fit rounded-xl"}`} onClick={()=>setexpanded(!expanded)}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="flex bi bi-pencil-square w-6 pt-2" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                </svg>
+                <h1 class="ml-3 mr-6 pt-1">Notes</h1>
+                <svg class="bi bi-arrows-angle-expand ml-auto w-4 pt-2" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                    {expIcon()}
                 </svg>
             </div>
             <NotesDisplay show={expanded}/>
--- pomodoro-app/src/components/Notes/NotesDisplay.jsx
+++ pomodoro-app/src/components/Notes/NotesDisplay.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MinimizedNote(props){
+    return (
+        <div class="flex flex-col w-fill px-2 pt-2 hover:cursor-pointer hover:bg-gray-300" onClick={props.expand}>
+            <div class="flex">
+                <label class="w-20 font-medium border-white border-r-[2px] pr-2 hover:cursor-pointer">{props.date}</label>
+                <p class="line-clamp-1 pl-2">{props.note}</p>
+            </div>
+            <hr class="border-[1.5px] rounded-full mt-1" />
+        </div>
+    );
+}
+
+
+function generateNotes(expandNote) {
+    let notes = [];
+    
+    for(let i=0; i<5; i++){
+        notes.push(
+            <MinimizedNote date={"11/2/2024"} expand={()=>expandNote(i)} note="super long test message. this is a long note to overflow the box. Lots of text here."/>
+        )
+    }
+
+    return notes;
+}
 
 
 export default function NotesDisplay(props){
+    const [expandNote, setExpandNote] = useState(null);
 
     return props.show?(
         <div class="flex flex-col h-full w-full bg-gray-400 rounded-b-xl">
-            
+            {generateNotes(setExpandNote)}
         </div>
     ): "";
 }
