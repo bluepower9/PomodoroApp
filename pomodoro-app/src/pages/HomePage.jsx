a483d8daae6f2e701345f16ee660bf70e72d32e2
bluepower9 <jarrod.leong@yahoo.com>
2024-11-22 04:19:46 -0800

    increased width of app slightly to make more room for side widgets

--- pomodoro-app/src/pages/HomePage.jsx
+++ pomodoro-app/src/pages/HomePage.jsx
@@ -21,7 +21,7 @@ export default function HomePage() {
         <div class="flex flex-col justify-center bg-black font-sans min-h-screen">
             <Settings settingsModalRef={settingsModalRef} phases={phases} setPhases={setPhases} />
             <NavBar settingsModalRef={settingsModalRef} />
-            <div class="flex w-full justify-stretch mt-16 py-8 max-w-[90rem] mx-auto px-8">
+            <div class="flex w-full justify-stretch mt-16 py-8 max-w-[90vw] mx-auto px-8">
                 <div class="flex flex-1 min-w-80">
                     <Notes />
                 </div>
